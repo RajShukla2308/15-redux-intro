90812d202b275b6773e22070b5db00c1bc1953ca
Raj Shukla <rajshukla9630@gmail.com>
2026-05-29 16:18:01 +0530

    L-269 legacy way of connecting components to redux

--- src/features/accounts/BalanceDisplay.js
+++ src/features/accounts/BalanceDisplay.js
@@ -1,3 +1,5 @@
+import { connect } from "react-redux";
+
 function formatCurrency(value) {
   return new Intl.NumberFormat("en", {
     style: "currency",
@@ -5,8 +7,19 @@ function formatCurrency(value) {
   }).format(value);
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+
+// C - accept props
+function BalanceDisplay({balance}) {
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+
+// A - create function
+function mapStateToProps(state){
+  return {
+    balance: state.account.balance
+  }
+}
+
+// B - export
+export default connect(mapStateToProps)(BalanceDisplay);
