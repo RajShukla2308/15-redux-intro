23efe18240e52cbbc23acdf5adb940910966c1be
Raj Shukla <rajshukla9630@gmail.com>
2026-05-29 16:07:51 +0530

    L-267-268 connecting and working on redux

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 
 
 import store from './Store';
 
-store.dispatch({type:'account/deposit',payload: 500});
-console.log(store.getState());
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
