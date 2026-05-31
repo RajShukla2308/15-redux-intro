9b5a7545d1579d0c01ed053d8630a91159f69bee
Raj Shukla <rajshukla9630@gmail.com>
2026-05-31 09:46:54 +0530

    L-278 converting to redux

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,10 +1,41 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
     fullName: '',
     nationalID: '',
     createdAt: ''
 }
 
 
+// Redux toolkit way to do the things
+const customerSlice = createSlice({
+    name: 'customer',
+    initialState,
+    reducers:{
+        createCustomer: {
+            prepare(fullName, nationalID){
+                return {
+                    payload: {fullName, nationalID}
+                }
+            },
+
+            reducer(state, action){
+                state.fullName = action.payload.fullName;
+                state.nationalID = action.payload.nationalID;
+                state.createdAt = new Date().toISOString();
+            }
+        },
+        updateName(state,action){
+            state.fullName = action.payload;
+        }
+    }
+}) 
+
+export const {createCustomer, updateName} = customerSlice.actions;
+export default customerSlice.reducer;
+
+
+/*
 export default function customerReducer(state = initialStateCustomer, action){
     switch(action.type){
         case 'customer/createCustomer':
@@ -33,6 +64,7 @@ export function createCustomer(fullName, nationalID){
 export function updateName(fullName){
     return {type: 'customer/updateName', payload: fullName }
 }
+*/
 
 
 // store.dispatch(createCustomer('Raj','1234'));
